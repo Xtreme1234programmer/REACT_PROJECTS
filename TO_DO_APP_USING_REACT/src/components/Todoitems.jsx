7e56476f0a6ea53f1beeff804b324aa04b82d954
Pankaj singh negi <99879926+Xtreme1234programmer@users.noreply.github.com>
2024-09-05 16:24:01 +0530

    first commit

---
+++ TO_DO_APP_USING_REACT/src/components/Todoitems.jsx
@@ -0,0 +1,19 @@
+//to run uncomment all lines
+import TodoItem from "./TodoItem";
+import styles from "./TodoItems.module.css";
+
+const TodoItems = ({ todoItems, onDeleteClick }) => {
+  return (
+    <div className={styles.itemsContainer}>
+      {todoItems.map((item) => (
+        <TodoItem
+          todoDate={item.dueDate}
+          todoName={item.name}
+          onDeleteClick={onDeleteClick}
+        ></TodoItem>
+      ))}
+    </div>
+  );
+};
+
+export default TodoItems;
